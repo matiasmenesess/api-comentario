bd4bbc58cf998c9b703b0c0e55753230ee58fdcb
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-04 09:45:46 -0500

    Update comentario.py

--- comentario.py
+++ comentario.py
@@ -1,6 +1,7 @@
 import boto3
 import uuid
 import os
+import json
 
 def lambda_handler(event, context):
     # Entrada (json)
@@ -8,6 +9,8 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    bucket_name = os.environ["INGEST_BUCKET_NAME"]
+    
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -17,9 +20,15 @@ def lambda_handler(event, context):
           'texto': texto
         }
     }
+    
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+
+    s3_client = boto3.client('s3')
+    s3_key = f"{tenant_id}/{uuidv1}.json"
+    s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=json.dumps(comentario))
+    
     # Salida (json)
     print(comentario)
     return {
